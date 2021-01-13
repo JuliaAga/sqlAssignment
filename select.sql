9b048430f5fee81984a47b8268727ef7d5f61ff9
Yuliia <dongro@yandex.ru>
2021-01-13 20:30:11 +0500

    fix select with median and formatting lines

--- select.sql
+++ select.sql
@@ -2,36 +2,37 @@
 --имя работодателя для первых 10 вакансий у которых не указана зарплата, 
 --сортировать по дате создания вакансии от новых к более старым.
 SELECT 
-	vacancy.name AS Vacancy, 
-	location.name as City, 
-	employer.name AS Employer
+    vacancy.name AS Vacancy, 
+    location.name as City, 
+    employer.name AS Employer
 FROM vacancy 
 LEFT JOIN location ON vacancy.location_id = location.id
 LEFT JOIN employer ON vacancy.employer_id = employer.id
 WHERE 
-	compensation_from IS null 
-	AND compensation_to IS NULL
+    compensation_from IS null 
+    AND compensation_to IS NULL
 ORDER BY 
-	date_create DESC
+    date_create DESC
 LIMIT 10
 
 --Вывести среднюю максимальную зарплату в вакансиях, среднюю минимальную и среднюю среднюю 
 --(compensation_to - compensation_from) в одном запросе. Значения должны быть указаны до вычета налогов.
 WITH cte_compensataion AS (
-select 
-	CASE WHEN compensation_gross IS true
-    	THEN (compensation_from*0.87)
-    	ELSE compensation_from
-	END AS minimum, 	
-	CASE WHEN compensation_gross IS true
-    	THEN (compensation_to*0.87)
-    	ELSE compensation_to
-	END AS maximum
-FROM vacancy)
+    SELECT 
+        CASE WHEN compensation_gross IS true
+            THEN (compensation_from*0.87)
+            ELSE compensation_from
+        END AS minimum,     
+        CASE WHEN compensation_gross IS true
+            THEN (compensation_to*0.87)
+            ELSE compensation_to
+        END AS maximum
+    FROM vacancy
+)
 SELECT 
-	ROUND (AVG(maximum), 2) AS average_maximum,
-	ROUND (AVG(minimum), 2) AS average_minimum,
-	ROUND (AVG((maximum+minimum)/2), 2) AS average
+    ROUND (AVG(maximum), 2) AS average_maximum,
+    ROUND (AVG(minimum), 2) AS average_minimum,
+    ROUND (AVG((maximum+minimum)/2), 2) AS average
 FROM cte_compensataion
 
 
@@ -39,23 +40,23 @@ FROM cte_compensataion
 --в порядке убывания откликов. Если более 5 компаний получили одинаковое максимальное 
 --количество откликов, отсортировать по алфавиту и вывести только 5.
 WITH cte_count_response AS (
-SELECT 
-	employer_id,
-	max(quantity) AS max_quantity
-FROM (
-	SELECT 
-		employer_id, 
-		vacancy_id, 
-		COUNT(response.id) AS quantity 
-	FROM response
-	LEFT JOIN vacancy on response.vacancy_id = vacancy.id
-	GROUP BY employer_id, vacancy_id 
-) AS inner_table
-GROUP BY employer_id
-ORDER BY max_quantity DESC
-	)
+    SELECT 
+        employer_id,
+        max(quantity) AS max_quantity
+    FROM (
+        SELECT 
+            employer_id, 
+            vacancy_id, 
+            COUNT(response.id) AS quantity 
+        FROM response
+        LEFT JOIN vacancy on response.vacancy_id = vacancy.id
+        GROUP BY employer_id, vacancy_id 
+    ) AS inner_table
+    GROUP BY employer_id
+    ORDER BY max_quantity DESC
+)
 SELECT
-	name
+    name
 FROM cte_count_response AS cte
 LEFT JOIN employer AS emp ON cte.employer_id = emp.id
 ORDER BY max_quantity DESC, name
@@ -64,28 +65,31 @@ LIMIT 5
 --Вывести медианное количество вакансий на компанию. Использовать percentile_cont.
 WITH cte_quantity AS (
     SELECT 
-		count(id) AS quantity,
-		employer_id
-    FROM vacancy 
-    GROUP BY employer_id
+        count(vac.id) AS quantity,
+        em.id AS Employer_ID
+    FROM employer AS em
+    LEFT JOIN vacancy AS vac ON em.id = vac.employer_id
+    GROUP BY em.id
 ) 
 SELECT 
-	percentile_cont(0.5) WITHIN GROUP (ORDER BY quantity) as median
+    percentile_cont(0.5) WITHIN GROUP (ORDER BY quantity) as median
 FROM cte_quantity;
 
 --Вывести минимальное и максимальное время от создания вакансии до первого отклика для каждого города.
 WITH firstResponse AS (
-    SELECT vac.id vac_id,
-           loc.name,
-           loc.id loc_id,
-           min(resp.date_create - vac.date_create) AS timeToFirst
+    SELECT 
+        vac.id vac_id,
+        loc.name,
+        loc.id loc_id,
+        min(resp.date_create - vac.date_create) AS timeToFirst
     FROM response resp
-        LEFT JOIN vacancy vac ON resp.vacancy_id = vac.id
-        LEFT JOIN location loc ON vac.location_id = loc.id
+    LEFT JOIN vacancy vac ON resp.vacancy_id = vac.id
+    LEFT JOIN location loc ON vac.location_id = loc.id
     GROUP BY vac.id , loc.id
 )
-SELECT name,
-       max(timeToFirst),
-       min(timeToFirst)
+SELECT 
+    name,
+    max(timeToFirst),
+    min(timeToFirst)
 FROM firstResponse
 GROUP BY loc_id, name;
